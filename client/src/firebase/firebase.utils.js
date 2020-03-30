46e813ba77da8d0eba26f6665d57d9cb7712812d
Lidii <lidiiany92@gmail.com>
2020-03-30 16:49:07 -0700

    stroring user data in the firebase

--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -14,6 +14,29 @@ var config = {
   measurementId: "G-M49STQKJK4"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
