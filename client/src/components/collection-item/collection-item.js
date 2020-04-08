1fb98dcdc4feb37b51de2a637b110633a3b6165a
Lidii <lidiiany92@gmail.com>
2020-04-08 13:39:31 -0700

    style changes to collection item

--- client/src/components/collection-item/collection-item.js
+++ client/src/components/collection-item/collection-item.js
@@ -1,4 +1,5 @@
 import React from "react";
+import CustomButton from "../custom-button/custom-button";
 
 import "./collection-item.scss";
 
@@ -14,6 +15,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
       <span className="name">{name}</span>
       <span className="price">{price}</span>
     </div>
+    <CustomButton inverted>Add to cart</CustomButton>
   </div>
 );
 
